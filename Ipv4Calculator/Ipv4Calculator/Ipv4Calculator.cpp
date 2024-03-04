c3712d914041f595f113ad74dd06ac40a5a0776f
Krystian <krystianpochopien@wp.pl>
2024-03-05 00:32:49 +0100

    Update

--- Ipv4Calculator/Ipv4Calculator/Ipv4Calculator.cpp
+++ Ipv4Calculator/Ipv4Calculator/Ipv4Calculator.cpp
@@ -1,62 +1,60 @@
 ﻿#include <iostream>
 #include <bitset>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	cout << "Type IP Address: \n";
+	
+	string IPString;
+	getline(cin, IPString);
 
-
-	cout << "Wpisz adres IP (Po spacji!): "										<< endl;
-	int decIP[4];
-
-	cin >> decIP[0] >> decIP[1] >> decIP[2] >> decIP[3];
+	stringstream ExtractedIPAdress(IPString);
+	int decIP[4] = {0};
+	char delimiter;
 
 	system("cls");
 
-	cout << decIP[0] << "." << decIP[1] << "." << decIP[2] << "." << decIP[3]	<< endl;
+	cout << "Entered IP Address: \n";
+	ExtractedIPAdress >> decIP[0] >> delimiter >> decIP[1] >> delimiter >> decIP[2] >> delimiter >> decIP[3];
+	cout << decIP[0] << "." << decIP[1] << "." << decIP[2] << "." << decIP[3] << "\n";
 
-	cout << "======================="											<< endl;
-
-	bitset<8> bin_ip(decIP[0]);
-	cout << bin_ip << ".";
-
-	bitset<8> bin_ip1(decIP[1]);
-	cout << bin_ip1 << ".";
-
-	bitset<8> bin_ip2(decIP[2]);
-	cout << bin_ip2 << ".";
-
-	bitset<8> bin_ip3(decIP[3]);
-	cout << bin_ip3																<< endl;
-
+	for (int i = 0; i < 4; ++i) {
+		bitset<8> ConvertedToDecimal(decIP[i]);
+		cout << ConvertedToDecimal;
+		if (i < 3) {
+			cout << ".";
+		}
+	}
 
 	if (decIP[0] <= 126)
 	{
-		cout << "Ip nalezy do klasy A"											<< endl;
-		cout << "Maska to: 255.0.0.0"											<< endl;									
+		cout << "\nAddress belongs to the class A \n";
+		cout << "Subnet mask: 255.0.0.0 \n";									
 	}
-	else if (decIP[0] > 127, decIP[0] <= 191)
+	else if (decIP[0] > 127 && decIP[0] <= 191)
 	{
-		cout << "Ip nalezy do klasy B"											<< endl;
-		cout << "Maska to: 255.255.0.0" << endl;
+		cout << "\nAddress belongs to the class B \n";
+		cout << "Subnet mask: 255.255.0.0 \n";
 	}
-	else if (decIP[0] > 191, decIP[0] <= 223)
+	else if (decIP[0] > 191 && decIP[0] <= 223)
 	{
-		cout << "Ip nalezy do klasy C"											<< endl;
-		cout << "Maska to: 255.255.255.0"										<< endl;
+		cout << "\nAddress belongs to the class C \n";
+		cout << "Subnet mask: 255.255.255.0 \n";
 	}
-	else if (decIP[0] > 223, decIP[0] <= 239)
+	else if (decIP[0] > 223 && decIP[0] <= 239)
 	{
-		cout << "Ip nalezy do klasy D"											<< endl;
-		cout << "Maska to: BRAK"												<< endl;
+		cout << "\nAddress belongs to the class D \n";
+		cout << "Subnet mask: None \n";
 	}
-	else if (decIP[0] > 240, decIP[0] <= 247)
+	else if (decIP[0] > 240 && decIP[0] <= 247)
 	{
-		cout << "Ip nalezy do klasy E"											<< endl;
-		cout << "Maska to: BRAK"												<< endl;
+		cout << "\nAddress belongs to the class E \n";
+		cout << "Subnet mask: None \n";
 	}
-
-
+	
 	return 0;
 }
